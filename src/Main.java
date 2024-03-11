85dbd02bfd0cf811222b34318b075d00e003c3b2
Sergey White <sergey_white86@mail.ru>
2024-03-11 19:55:46 +0300

    доработал Task1 и Task4

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,9 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        List<Integer> nums = new ArrayList<>(List.of(1, 1, 2, 3, 4, 4, 5, 5, 6, 7));
+        List<Integer> nums = new ArrayList<>(List.of(-3, 1, 1, 2, 3, 4, 4, 5, 5, 6, 7));
         List<String> words = new ArrayList<>(List.of("ball", "sun", "apple", "cat", "cat", "sun"));
-        List<String> strings = new ArrayList<>(List.of("один", "один", "два", "два", "два", "три", "три", "три"));
+        List<String> strings = new ArrayList<>(List.of("один", "один", "два", "два", "два", "три", "три", "три", "четыре"));
         System.out.println("Task1");
         task1(nums);
         System.out.println();
@@ -19,7 +19,7 @@ public class Main {
     }
 
     public static void task1(List<Integer> nums) {
-        nums.stream().filter(val -> val % 2 == 1).forEach(val -> System.out.printf("%d ", val));
+        nums.stream().filter(val -> val % 2 != 0).forEach(val -> System.out.printf("%d ", val));
     }
 
     public static void task2(List<Integer> nums) {
@@ -39,7 +39,9 @@ public class Main {
             countWord.put(word, frequency);
         }
         for (Map.Entry<String, Integer> word : countWord.entrySet()) {
-            System.out.printf("%s - %d повторений/я ", word.getKey(), word.getValue());
+            if (word.getValue() > 1) {
+                System.out.printf("%s - %d повторений/я ", word.getKey(), word.getValue());
+            }
         }
     }
 }
